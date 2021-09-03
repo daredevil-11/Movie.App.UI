23dd10f7602a501eb88b140e5037b5b8620b4b16
Tanmay Das <tanmay-das@hotmail.com>
2021-09-04 02:58:01 +0530

    ui app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'App',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44320',
+    redirectUri: baseUrl,
+    clientId: 'MovieApp_UI',
+    responseType: 'code',
+    scope: 'offline_access MovieApp',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44320',
+      rootNamespace: 'Movie.App',
+    },
+  },
+} as Environment;
